efadc0bd28bdf80834f8d0d1a89cabb771232f83
Markussim <markus.simonsen@gmail.com>
2020-02-20 10:51:40 +0100

    Added commands in command.rs. Does not work

--- src/command.rs
+++ src/command.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, cloudyyyyyyyyyyyyyyyy world!");
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(&["/C", "echo hello"])
+                .output()
+                .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("echo hello")
+                .output()
+                .expect("failed to execute process")
+    };
+    
+    let hello = output.stdout;
+
+    println!("{:?}", hello);
 }
