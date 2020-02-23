6cad4c0e151bdaee95ca92abd26fb0b4831fe0f4
Anton <anrehelenius@gmail.com>
2020-02-23 12:10:17 +0100

    Update command.rs

--- src/command.rs
+++ src/command.rs
@@ -3,13 +3,13 @@ use std::process::Command;
 fn main() {
     let output = if cfg!(target_os = "windows") {
         Command::new("cmd")
-                .args(&["/C", "echo hello"])
+                .args(&["/C", "echo hello Windows!"])
                 .output()
                 .expect("failed to execute process")
     } else {
         Command::new("sh")
                 .arg("-c")
-                .arg("echo hello")
+                .arg("echo hello not windows!")
                 .output()
                 .expect("failed to execute process")
     };
