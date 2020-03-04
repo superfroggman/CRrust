a6ff8f4adacbf70fdd0dc32e36ac4e080a1c2d7c
Anton <anrehelenius@gmail.com>
2020-03-04 12:34:08 +0100

    many things
    Co-authored-by: linusromland <romlandlinus@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::env;
+extern crate num_cpus;
+extern crate compiler_builtins;
+
+// count logical cores this process could try to use
+let num = num_cpus::get();
+
+
 fn main() {
-    println!("Hello, cloudyyyyyyyyyyyyyyyy world!");
+    //intro!
+    println!("CRust! by superfroggman, Markussim & linusromland");
+
+    //declare variables
+    let mut input = "";
+    let mut output = "";
+
+    //Get arguments
+    let args: Vec<_> = env::args().collect();
+
+    //Check arguments
+    for x in 1..args.len(){
+        //help
+        if args[x]=="-h"||args[x]=="--help" {
+            println!("\nCRrust is a cloud remover. Import mp4, gif of the sky and remove the clouds. \n\n-i (--input) input file location \n\n-o (--output) output file location\n");
+        
+            return;
+        }
+
+        //input file
+        if (args[x]=="-i" || args[x]=="--input") && args.len()>=x+2 {
+            let ha = x+1;
+            input = &args[ha];
+        }
+
+        
+        //out+put file
+        if (args[x]=="-o" || args[x]=="--output") && args.len()>=x+2 {
+            let ha = x+1;
+            output = &args[ha];
+        }
+    }
+
+    println!("Input file: {}",input);
+    println!("Output file: {}",output);
 }
